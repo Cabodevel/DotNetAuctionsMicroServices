ced4ebeedb9c9fcc1400fefea383a5f7e9bd3e0c
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-08-30 20:11:57 +0200

    Initial commit

---
+++ Backend/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,48 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/auctions")]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
+
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetById(Guid id)
+        {
+            var auction = await _context.Auctions
+               .Include(x => x.Item)
+               .FirstOrDefaultAsync();
+            
+            if(auction == null)
+            {
+                return BadRequest("Auction not found");
+            }
+
+            return _mapper.Map<AuctionDto>(auction);
+        }
+    }
+}
