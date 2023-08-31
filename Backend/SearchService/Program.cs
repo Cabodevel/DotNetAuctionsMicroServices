f2d15f74385e21e96fb818e6b4a18cd35abe5e72
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-08-31 20:22:29 +0200

    adding rabbitmq

--- Backend/SearchService/Program.cs
+++ Backend/SearchService/Program.cs
@@ -1,5 +1,7 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -8,12 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddHttpClient<AuctionSvcHttpClient>()
     .AddPolicyHandler(GetPolicy());
 
-
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["Rabbitmq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration["Rabbitmq:User"]);
+            h.Password(builder.Configuration["Rabbitmq:Passw"]);
+        });
+    });
+});
 
 var app = builder.Build();
 
