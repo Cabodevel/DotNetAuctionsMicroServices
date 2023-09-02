086a9c8c0a7bb9596757d1f758f229dcd4ec2abc
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-09-02 18:27:48 +0200

    adding auth

---
+++ Backend/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        
+        [Required]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string FullName { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public string Button { get; set; }
+    }
+}
